eb88580ed64e14b2a9918b1d0595d3c880f72545
ahamasmiyodha4-spec <ahamasmiyodha4@gmail.com>
2026-06-25 16:33:34 +0530

    Configure base path for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/dadar_electronics',
+  assetPrefix: '/dadar_electronics/',
 };
 
 export default nextConfig;
